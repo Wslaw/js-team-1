28d5f8ecbae9907a86e56b8444af55761687db34
DariaNovakoova <novakova2003dasha@gmail.com>
2023-09-16 11:20:45 +0300

    task2-done

--- js/lesson1.js
+++ js/lesson1.js
@@ -25,3 +25,18 @@
 
 // console.log("10" - 8 + true);
 // console.log(5 + 5 + "5");
+
+// ---------------------------------
+
+//Напишіть программу, яка отримує від користувача
+//число (кількість хвилин) и виводить у консоль
+//рядок у форматі годин та хвилин
+// https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/String/padStart
+// 70 === 01:10
+
+// const num = prompt("Enter number");
+// let hours = Math.floor(num / 60);
+// let minutes = num % 60;
+// hours = String(hours).padStart(2, "0");
+// minutes = String(minutes).padStart(2, "0");
+// console.log(`${hours}:${minutes}`);
