723524f850969f2e806a950082ddd51652e462a0
DariaNovakoova <novakova2003dasha@gmail.com>
2023-09-30 11:27:49 +0300

    task3-2

--- js/lesson2.js
+++ js/lesson2.js
@@ -29,18 +29,77 @@
 //3 замінить значення premium на false
 //4 виводить зміст об'єкта users у форматі
 //ключ:значення використовуя Object.keys() та for...of
-const user = {
-  name: "John",
-  age: 20,
-  hobby: "tenis",
-  premium: true,
-};
+// const user = {
+//   name: "John",
+//   age: 20,
+//   hobby: "tenis",
+//   premium: true,
+// };
 
-user.mood = "happy";
-user.hobby = 'skydiving';
-user.premium = false;
+// user.mood = "happy";
+// user.hobby = 'skydiving';
+// user.premium = false;
 
-const arr = Object.keys(user);
-for (const userKey of arr) {
-    console.log(`${userKey}: ${user[userKey]}`);
-}
+// const arr = Object.keys(user);
+// for (const userKey of arr) {
+//     console.log(`${userKey}: ${user[userKey]}`);
+// }
+
+// ------------------------
+
+//2. У нас є об'єкт, в якому зберігаються зарплати
+//нашої команди
+//Напишіть код для додавання усіх зарплат та
+//збережіть його результат в змінній sum.
+//Якщо об'єкт salaries пустий, то результат має бути 0
+
+// const salaries = {
+//   Mango: 100,
+//   Poly: 160,
+//   Ajax: 1470,
+// };
+
+// // let sum = 0;
+// const values = Object.values(salaries);
+// console.log(values);
+// // for (const value of values) {
+//     // sum += value;
+// //
+// // };
+// const sum = values.reduce((sum, value) => sum + value, 0);
+
+// console.log(sum);
+
+// ---------------------------------
+
+//3. Напишіть ф-цію calcTotalPrice(stones, stonesName),
+//яка приймає массив об'єктів і
+//рядок з назвами каміння.
+//Функція рахує і повертає загальну вартість каменів
+//з таким ім'ям, ціною та кількістю з об'єкта
+
+// const stones = [
+//   { name: "Смарагд", price: 1300, quantity: 4 },
+//   { name: "Діамант", price: 2700, quantity: 6 },
+//   { name: "Сапфір", price: 400, quantity: 7 },
+//   { name: "Щебінь", price: 150, quantity: 100 },
+// ];
+
+// function calcTotalPrice(stones, stonesName) {
+//   // let sum = 0;
+//   // for (const stone of stones) {
+//   //   if (stonesName === stone.name) {
+//   //     sum += stone.price * stone.quantity;
+//   //   }
+//   // }
+
+//   // return sum;
+
+//   return stones.reduce(
+//     (sum, stone) =>
+//       stone.name === stonesName ? sum + stone.price * stone.quantity : sum,
+//     0
+//   );
+// }
+
+// console.log(calcTotalPrice(stones, "Сапфір"));
